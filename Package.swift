16ddd37878e9bb55fb11ff610521b7e856ed7532
cloke <cory@me.com>
2026-01-05 19:05:48 -0500

    Make TaskRunner macOS-only
    TaskRunner uses Process API which is only available on macOS, not iOS.
    Changed platform from [.macOS("26"), .iOS("26")] to [.macOS("26")]

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TaskRunner",
-    platforms: [.macOS(.v12), .iOS(.v15)],
+    platforms: [.macOS("26")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
